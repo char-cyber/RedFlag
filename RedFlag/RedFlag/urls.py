ddaf91ccbabff18df340672d6bcec7ff3b7d97fb
henrynh2105 <hnh2105@gmail.com>
2025-11-08 13:25:26 -0600

    Home view

--- RedFlag/RedFlag/urls.py
+++ RedFlag/RedFlag/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('RedFlagApp/', include('RedFlagApp.urls')), 
+    path('RedFlagApp/', include('RedFlagApp.urls')),
 ]
--- RedFlag/RedFlagApp/views.py
+++ RedFlag/RedFlagApp/views.py
@@ -1,17 +1,20 @@
 # myapp/views.py
 from django.shortcuts import render
 from .forms import UploadFileForm
-from .models import MyFileModel # Assuming you have a model to store file info
+# from .models import MyFileModel # Assuming you have a model to store file info
 
-def upload_file_view(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Process the file here
-            uploaded_file = request.FILES['file']
-            # Example: Save to a model
-            MyFileModel.objects.create(title=form.cleaned_data['title'], file=uploaded_file)
-            return render(request, 'success.html')
-    else:
-        form = UploadFileForm()
-    return render(request, 'upload.html', {'form': form})
+def home(request):
+    return render(request, 'home.html')
+
+# def upload_file_view(request):
+#     if request.method == 'POST':
+#         form = UploadFileForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # Process the file here
+#             uploaded_file = request.FILES['file']
+#             # Example: Save to a model
+#             MyFileModel.objects.create(title=form.cleaned_data['title'], file=uploaded_file)
+#             return render(request, 'success.html')
+#     else:
+#         form = UploadFileForm()
+#     return render(request, 'upload.html', {'form': form})
